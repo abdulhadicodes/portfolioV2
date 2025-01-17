b5a472bec02350ae617477d88f604602ced60e19
Abdul Hadi <abdulhaadi.76543@gmail.com>
2025-01-17 23:06:26 +0500

    testo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import "./globals.css";
-
-import Header from "../components/Header.jsx";
-import Hero from "../components/Hero.jsx";
-import Intro from "../components/Intro.jsx";
-import Services from "../components/Services.jsx";
-import Contact from "../components/Contact.jsx";
-import Footer from "../components/Footer.jsx";
+import Header from "../../components/Header.jsx";
+import Hero from "../../components/Hero.jsx";
+import Intro from "../../components/Intro.jsx";
+import Services from "../../components/Services.jsx";
+import Contact from "../../components/Contact.jsx";
+import Footer from "../../components/Footer.jsx";
+import TestimonialSection from "../../components/TestimonialSection.jsx";
 import React from 'react';
 
 
@@ -17,9 +17,9 @@ function Home(){
     <Hero />
     <Intro />
     <Services/>
+    <TestimonialSection />
     <Contact/>
     <Footer/>
-  
     </>
     
   );
